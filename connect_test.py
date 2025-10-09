735d6ec6fa72bd50a070b17a2fb5aa97ff2ed1bc
OluwadamilareSunmola <144754597+OluwadamilareSunmola@users.noreply.github.com>
2025-10-09 04:41:39 -0500

    Implement telemetry logging in connect_test.py
    Added telemetry logging to connect_test.py.

--- connect_test.py
+++ connect_test.py
@@ -1,9 +1,24 @@
 from pymavlink import mavutil
 
-# Connect to SITL's MAVLink interface
+
 the_connection = mavutil.mavlink_connection('udp:127.0.0.1:14550')
 
-
-print("Waiting for heartbeat from ArduCopter...")
+print("Waiting for heartbeat...")
 the_connection.wait_heartbeat()
-print(f" Heartbeat received from system {the_connection.target_system}, component {the_connection.target_component}")
+print(f"Heartbeat received from system {the_connection.target_system}, component {the_connection.target_component}")
+
+
+with open("telemetry_log.txt", "w") as f:
+    while True:
+        msg = the_connection.recv_match(blocking=True)
+        if msg:
+            f.write(str(msg) + "\n")
+            f.flush()
+
+"""
+MAVLink is a lightweight messaging protocol designed for communication with drones and robotic vehicles.
+Using the pymavlink library, we send an initial heartbeat message to activate the device. By default, the flight controller doesn't stream telemetry data until it detects an active connection.
+We send a dummy heartbeat from our computer to the device, which acts as a "wake-up call." This triggers the device to begin transmitting its actual telemetry data with real parameter values.
+
+
+"""
